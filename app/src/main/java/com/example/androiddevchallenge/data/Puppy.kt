57ac0dd4c118ecada7a0cb03725d35e00ab96ec2
sanjin <454575674@qq.com>
2021-03-01 21:07:55 +0800

    remove import

--- app/src/main/java/com/example/androiddevchallenge/data/Puppy.kt
+++ app/src/main/java/com/example/androiddevchallenge/data/Puppy.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.data
 
 import androidx.compose.material.icons.Icons
@@ -19,8 +34,7 @@ data class Puppy(
     var isAdopt: Boolean = false
 )
 
-enum class Sex(val text: String, val flag: ImageVector,val color:Color) {
+enum class Sex(val text: String, val flag: ImageVector, val color: Color) {
     Male("Male", Icons.Default.Male, Color(0xFF1DA1F2)),
-    Female("Female", Icons.Default.Female,Color(0xFFf44336))
+    Female("Female", Icons.Default.Female, Color(0xFFf44336))
 }
-
